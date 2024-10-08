0e0f5754432d86e471dad36dfdd9404ab6241139
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-10-08 15:40:39 +0100

    Check that tags are unique per user

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from .models import Tag
 from users.serializers import UserSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+    def validate(self, data):
+        """Validate that the tag is unique to the logged in User"""
+        user = data.get('user')
+        name = data.get('name')
+
+        existing_tag = Tag.objects.all().filter(name=name, user=user.id)
+
+        if existing_tag:
+            raise serializers.ValidationError(
+                {"name": "Tag name should be unique"})
+
+        return data
 
 
 class PopulatedTagSerializer(TagSerializer):
--- tags/urls.py
+++ tags/urls.py
@@ -4,5 +4,4 @@ from .views import TagListView, TagDetailView
 urlpatterns = [
     path('', TagListView.as_view()),
     path('<int:pk>/', TagDetailView.as_view())
-
 ]
